b5b13f8c02e3a4074fe86e6d9c8723b30854662a
ramxis <rameez.saleem@gmail.com>
2017-11-06 23:12:37 +0100

    token

--- Code/Samples/Raiden demo (Two laptops)/DemoRaidenToken.sol
+++ Code/Samples/Raiden demo (Two laptops)/DemoRaidenToken.sol
@@ -9,27 +9,27 @@ pragma solidity ^0.4.15;
 // https://github.com/ethereum/EIPs/issues/20
  contract ERC20Interface {
 	// Get the total token supply
-	function totalSupply() constant returns (uint256 totalSupply);  
+	function totalSupply() constant returns (uint256 totalSupply);
     // Get the account balance of another account with address _owner
     function balanceOf(address _owner) constant returns (uint256 balance);
-   
+
     // Send _value amount of tokens to address _to
     function transfer(address _to, uint256 _value) returns (bool success);
-   
+
     // Send _value amount of tokens from address _from to address _to
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-   
+
     // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
     // If this function is called again it overwrites the current allowance with _value.
     // this function is required for some DEX functionality
      function approve(address _spender, uint256 _value) returns (bool success);
-   
+
     // Returns the amount which _spender is still allowed to withdraw from _owner
     function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-   
+
     // Triggered when tokens are transferred.
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-  
+
    // Triggered whenever approve(address _spender, uint256 _value) is called.
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
@@ -40,7 +40,7 @@ contract RaidenDemoToken is ERC20Interface {
 	uint8 public constant decimals = 18;
 	string public version = "0.1";
 	uint256 _totalSupply = 1000000;
-	
+
 	// Owner of this contract
     address public owner;
     // Balances for each account (Eth address mapping to ERC20 Token)
@@ -64,7 +64,7 @@ contract RaidenDemoToken is ERC20Interface {
     }
     //ERC20 Functions
 
-    function totalSupply() constant returns (uint256 balance) {
+    function totalSupply() constant returns (uint256 totalSupply) {
     	totalSupply = _totalSupply;
     }
 
@@ -76,7 +76,7 @@ contract RaidenDemoToken is ERC20Interface {
     //Transfer some tokens from owners account to the address _to
     function transfer(address _to, uint256 _amount) returns (bool success) {
     	if((balances[msg.sender]>=_amount) && (_amount >0) && (balances[_to] + _amount > balances[_to])) {
-    		
+
     		balances[msg.sender] -= _amount;
     		balances[_to] += _amount;
     		//call trasnefer event
@@ -98,7 +98,7 @@ contract RaidenDemoToken is ERC20Interface {
     .*/
     function transferFrom(address _from, address _to, uint256 _amount) returns (bool success) {
     	if (balances[_from]>=_amount && allowed[_from][msg.sender]>=_amount && _amount>0 && balances[_to] + _amount > balances[_to] ) {
-    		
+
     		balances[_from] -= _amount;
             allowed[_from][msg.sender] -= _amount;
             balances[_to] += _amount;
@@ -112,7 +112,7 @@ contract RaidenDemoToken is ERC20Interface {
     // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
     // If this function is called again it overwrites the current allowance with _value.
     function approve(address _spender, uint256 _amount) returns (bool success) {
-        
+
         allowed[msg.sender][_spender] = _amount;
         Approval(msg.sender, _spender, _amount);
         return true;
@@ -121,7 +121,7 @@ contract RaidenDemoToken is ERC20Interface {
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
          return allowed[_owner][_spender];
     }
-  
+
     //ERC20 Events
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
